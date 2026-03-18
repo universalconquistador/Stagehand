92398bd3fa77df9cb76e1be36e4ccdf6c61264c8
UniversalConquistador <universalconquistador@gmail.com>
2026-03-18 14:14:16 -0700

    CI: Adds Dalamud repo generation.

---
+++ MakeDalamudRepoTool.cs
@@ -0,0 +1,36 @@
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+//
+// MakeDalamudRepoTool
+//
+// Takes a Dalamud plugin manifest JSON and adapts it into a single-plugin Dalamud repository JSON.
+//
+
+const string IconUrl = "https://github.com/universalconquistador/Stagehand/releases/latest/download/icon.png";
+const string DownloadUrl = "https://github.com/universalconquistador/Stagehand/releases/latest/download/latest.zip";
+
+if (args.Length != 2)
+{
+    Console.WriteLine("Error: Expected two arguments.");
+    Console.WriteLine("  Usage: dotnet run MakeDalamudRepoTool.cs <pluginJson> <outRepoJson>");
+    return 1;
+}
+else
+{
+    var pluginJsonPath = args[0];
+    var outputPath = args[1];
+
+    JsonNode pluginNode = JsonNode.Parse(System.IO.File.ReadAllText(pluginJsonPath));
+    pluginNode["IconUrl"] = IconUrl;
+    pluginNode["DownloadLinkInstall"] = DownloadUrl;
+    pluginNode["DownloadLinkTesting"] = DownloadUrl;
+    pluginNode["DownloadLinkUpdate"] = DownloadUrl;
+    JsonNode outputNode = new JsonArray(pluginNode);
+
+    System.IO.File.WriteAllText(outputPath, outputNode.ToJsonString(new JsonSerializerOptions { WriteIndented = true }));
+
+    return 0;
+}
